6775af0c37af0f8c023a3d3df26c5a971276f14b
Bbondchen <128323709+Bbondchen@users.noreply.github.com>
2023-11-10 22:09:58 +0100

    init qr code generator project

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    { message: "type in your URL", name: "URL" },
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+    // Use user feedback for... whatever!!
+
+    fs.writeFile("url.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
